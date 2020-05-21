99fe91a3f95a94e0ebdf5e52bffc9f238b04c6cb
danielvieira95 <engdanielvieira95@gmail.com>
2020-05-21 19:23:34 -0300

    Código para fazer a varredura no barramento I2C

---
+++ Scanner_I2C/Scanner_I2C.ino
@@ -0,0 +1,45 @@
+/*
+ * Código para realizar o monitoramento do barramento I2C do Arduino nano
+ */
+#include <Wire.h>
+ 
+void setup()  {
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("Scanner I2C");
+}
+ 
+void loop() {
+  byte erro, endereco;
+  int dispositivos;
+ 
+  Serial.println("Procurando Dispositivo I2C ...");
+ 
+  dispositivos = 0;
+ 
+  for (endereco = 1; endereco < 127; endereco++ )   {
+    Wire.beginTransmission(endereco);
+    erro = Wire.endTransmission();
+ 
+    if (erro == 0)  {
+      Serial.print("Dispositivo I2C encontrado. Endereco 0x");
+ 
+      if (endereco < 16)   Serial.print("0");
+      Serial.print(endereco, HEX);
+      Serial.println("  !");
+      dispositivos++;
+    }
+ 
+    else if (erro == 4)    {
+      Serial.print("Erro desconhecido no Endereco 0x");
+      if (endereco < 16)    Serial.print("0");
+    }
+  }
+ 
+  if (dispositivos == 0)
+    Serial.println("Nenhum dispositivo I2C encontrado\n");
+ 
+  else
+    Serial.println("Scaneamento Concluido\n");
+    while(Serial);
+}
